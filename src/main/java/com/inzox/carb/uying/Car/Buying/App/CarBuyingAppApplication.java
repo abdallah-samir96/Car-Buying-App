949f9f2369b5fbb6e6876082a15a81b67f8e0479
Abdallah Samir <abdallah01097261247@gmail.com>
2026-09-07 18:52:07 +0300

    building the project structure

---
+++ src/main/java/com/inzox/carb/uying/Car/Buying/App/CarBuyingAppApplication.java
@@ -0,0 +1,13 @@
+package com.inzox.carb.uying.Car.Buying.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBuyingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBuyingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inzox/carb/uying/Car/Buying/App/CarBuyingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inzox.carb.uying.Car.Buying.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBuyingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
